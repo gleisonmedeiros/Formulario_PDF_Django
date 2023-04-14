1154665257319e0c9560a4f90895b88913a7771f
Gleison <pgleison@yahoo.com.br>
2023-04-14 10:37:42 -0300

    Instalando a Configuracao da Railway2

--- Formulario/urls.py
+++ Formulario/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('hello', views.hello, name='hello'),
     path('sum', views.sum, name='sum'),
     path('form', views.form, name='form'),
+    path('', views.form, name='form'),
     path('download/', views.download_file, name='download_file'),
     path('limpar/', views.limpar_campos, name='limpar_campos'),
     path('json/', views.download_json, name='download_json'),
