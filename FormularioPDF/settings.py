b5cb2f66371fbc190914f293ba11ab94453689e2
Gleison <pgleison@yahoo.com.br>
2023-04-14 13:03:28 -0300

    Instalando a Configuracao da Railway 6

--- FormularioPDF/settings.py
+++ FormularioPDF/settings.py
@@ -51,7 +51,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # ...
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+
 ]
+
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ROOT_URLCONF = 'FormularioPDF.urls'
 
